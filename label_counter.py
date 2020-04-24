a79001d288c553c6ba4bf5363d07268de89b968a
psds075 <psds075@gmail.com>
2020-04-24 11:26:58 +0900

    label counter

---
+++ label_counter.py
@@ -0,0 +1,40 @@
+import os
+import pandas as pd
+import json
+import collections
+
+
+with open('env.json') as json_file:
+    data = json.load(json_file)
+    BASE_DIR = data['BASE_DIR']
+
+# 폴더명 확인
+    
+folderlist = []
+for DB_NAME in os.listdir(BASE_DIR):
+    if(os.path.isdir(BASE_DIR+DB_NAME)):
+        if not os.path.isfile(BASE_DIR+DB_NAME+'.xls'):
+            df = pd.DataFrame({'FILENAME':os.listdir(BASE_DIR+DB_NAME)})
+            df.to_excel(BASE_DIR+DB_NAME+'.xls', sheet_name='Sheet1', index = False, float_format=None)
+        folderlist.append(DB_NAME)
+
+print(folderlist)
+DATASET_NAME = folderlist[1]
+
+LABEL_LIST = []
+
+for forder in folderlist:
+    df = pd.read_excel(BASE_DIR+DATASET_NAME+'.xls', sheet_name='Sheet1', na_rep='')
+    for i in range(len(df)):
+        if not df['BBOX_LABEL'].isnull().iloc[i]:
+            BBOX_LABEL = json.loads(df['BBOX_LABEL'].iloc[i])
+            for j in range(len(BBOX_LABEL)):
+                LABEL_LIST.append(BBOX_LABEL[0]['label'])
+            
+DiseaseStatistics = collections.Counter(LABEL_LIST)
+print(DiseaseStatistics)
+
+#BBOX_LABEL = json.loads(df['BBOX_LABEL'].iloc[i])
+#BBOX_LABEL = json.loads(df['BBOX_LABEL'].iloc[0])
+#print(len(BBOX_LABEL))
+
