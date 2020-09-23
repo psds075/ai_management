12939150569578a757ee5eee0d05cfcc5a0f1bc3
최선 <amorsun@naver.com>
2020-09-23 17:07:32 +0900

    DB RATIO CHECK

--- db_control/db_precision.py
+++ db_control/db_precision.py
@@ -29,16 +29,19 @@ imagedata = DENTIQUB["imagedata"]
 dataset = DENTIQUB["dataset"]
 
 DICT_TRUETOTAL = dict()
-DICT_FALSETOTAL = dict()
-DICT_FALSENEGATIVE = dict()
-DICT_FALSEPOSITIVE = dict()
+DICT_NEGATIVETOTAL = dict()
+DICT_POSITIVETOTAL = dict()
 DICT_TRUENEGATIVE = dict()
 DICT_TRUEPOSITIVE = dict()
+
 DICT_SENSITIVITY = dict()
 DICT_SPECIFICITY = dict()
+DICT_PRECISION = dict()
 
+today = str(datetime.date.today())
 FROM_DATE = '2020-07-01'
-TO_DATE = '2020-09-20'
+TO_DATE = today
+
 
 #Sensitivity 평가
 for image in imagedata.find({'CONFIRM_CHECK':'CONFIRM'}):
@@ -53,115 +56,83 @@ for image in imagedata.find({'CONFIRM_CHECK':'CONFIRM'}):
             for LABEL in BBOX_LABEL:
                 if(not LABEL['label'] in DICT_TRUETOTAL):
                     DICT_TRUETOTAL[LABEL['label']] = 0
-                    DICT_FALSENEGATIVE[LABEL['label']] = 0
                     DICT_TRUEPOSITIVE[LABEL['label']] = 0
+                    DICT_NEGATIVETOTAL[LABEL['label']] = 0
+                    DICT_TRUENEGATIVE[LABEL['label']] = 0
+                    DICT_POSITIVETOTAL[LABEL['label']] = 0
                 DICT_TRUETOTAL[LABEL['label']] += 1
                 if(LABEL['label'] in PREDICTED_LABEL_SET):
                     DICT_TRUEPOSITIVE[LABEL['label']] += 1
         
 for label in DICT_TRUETOTAL:
-    DICT_SENSITIVITY[label] = int((DICT_TRUEPOSITIVE[label]*100) / (DICT_TRUETOTAL[label] + 0.01))
+    DICT_SENSITIVITY[label] = int((DICT_TRUEPOSITIVE[label]*100) / (DICT_TRUETOTAL[label] + 0.001))
     print("%s Sensitivity : %s %% (%s/%s)" % (label, str(DICT_SENSITIVITY[label]), str(DICT_TRUEPOSITIVE[label]), str(DICT_TRUETOTAL[label])))
+
+print()
+print()
 
 #Specificity 평가
 for image in imagedata.find({'CONFIRM_CHECK':'CONFIRM'}):
     if 'BBOX_PREDICTION' in image:
         if((image['TIMESTAMP'] > FROM_DATE) & (image['TIMESTAMP'] < TO_DATE)):
-            BBOX_LABEL_PREDICTION = json.loads(image['BBOX_PREDICTION'])
-            PREDICTED_LABEL_SET = set()
-            for LABEL in BBOX_LABEL_PREDICTION:
-                PREDICTED_LABEL_SET.add(LABEL['label'])
             
             BBOX_LABEL = json.loads(image['BBOX_LABEL'])
+            BBOX_PREDICTION = json.loads(image['BBOX_PREDICTION'])
+            GROUNDTRUTH_LABEL_SET = set()
+            PREDICTION_LABEL_SET = set()
             for LABEL in BBOX_LABEL:
-                if(not LABEL['label'] in DICT_TRUETOTAL):
-                    DICT_TRUETOTAL[LABEL['label']] = 0
-                    DICT_FALSENEGATIVE[LABEL['label']] = 0
-                    DICT_TRUEPOSITIVE[LABEL['label']] = 0
-                DICT_TRUETOTAL[LABEL['label']] += 1
-                if(LABEL['label'] in PREDICTED_LABEL_SET):
-                    DICT_TRUEPOSITIVE[LABEL['label']] += 1
-        
-for label in DICT_FALSETOTAL:
-    DICT_SENSITIVITY[label] = int((DICT_TRUEPOSITIVE[label]*100) / (DICT_TRUETOTAL[label] + 0.01))
-    print("%s Sensitivity : %s %% (%s/%s)" % (label, str(DICT_SENSITIVITY[label]), str(DICT_TRUEPOSITIVE[label]), str(DICT_TRUETOTAL[label])))
+                GROUNDTRUTH_LABEL_SET.add(LABEL['label'])
+            for LABEL in BBOX_PREDICTION:
+                PREDICTION_LABEL_SET.add(LABEL['label'])
+                
+            for DISEASE in list(DICT_TRUEPOSITIVE.keys()):
+                if(DISEASE not in GROUNDTRUTH_LABEL_SET):
+                    DICT_NEGATIVETOTAL[DISEASE] += 1
+                    if(DISEASE not in PREDICTION_LABEL_SET):
+                        DICT_TRUENEGATIVE[DISEASE] += 1
 
 
-'''
-        
-    if 'TIMESTAMP' in image:
-        
-        #if(True):
+for label in DICT_NEGATIVETOTAL:
+    DICT_SPECIFICITY[label] = int((DICT_TRUENEGATIVE[label]*100) / (DICT_NEGATIVETOTAL[label] + 0.001))
+    print("%s Specificity : %s %% (%s/%s)" % (label, str(DICT_SPECIFICITY[label]), str(DICT_TRUENEGATIVE[label]), str(DICT_NEGATIVETOTAL[label])))
+
+print()
+print()
+
+
+#Precision 평가
+for DISEASE in list(DICT_TRUEPOSITIVE.keys()):
+    DICT_TRUEPOSITIVE[DISEASE] = 0
+
+for image in imagedata.find({'CONFIRM_CHECK':'CONFIRM'}):
+    if 'BBOX_PREDICTION' in image:
         if((image['TIMESTAMP'] > FROM_DATE) & (image['TIMESTAMP'] < TO_DATE)):
+            
             BBOX_LABEL = json.loads(image['BBOX_LABEL'])
-            
+            BBOX_PREDICTION = json.loads(image['BBOX_PREDICTION'])
+            GROUNDTRUTH_LABEL_SET = set()
+            PREDICTION_LABEL_SET = set()
             for LABEL in BBOX_LABEL:
-                if(not LABEL['label'] in DICT_TOTAL):
-                    DICT_TOTAL[LABEL['label']] = 0
-                    DICT_WRONG[LABEL['label']] = 0
-                    DICT_RIGHT[LABEL['label']] = 0
-                DICT_TOTAL[LABEL['label']] += 1
-                DICT_RIGHT[LABEL['label']] += 1
-            
-            
-            # 전체 정확도
-            TOTAL+=1
-            if 'PREDICTION_CHECK' in image:
-                if (image['PREDICTION_CHECK'] == 'PREDICT'):
-                    RIGHT+=1
-                    if ('BBOX_LABEL' in image):
-                        if((image['BBOX_LABEL']) == ''):
-                            image['BBOX_LABEL'] = '[]'
-                        BBOX_LABEL = json.loads(image['BBOX_LABEL'])
-                        for LABEL in BBOX_LABEL:
-                            if(not LABEL['label'] in DICT_TOTAL):
-                                DICT_TOTAL[LABEL['label']] = 0
-                                DICT_WRONG[LABEL['label']] = 0
-                                DICT_RIGHT[LABEL['label']] = 0
-                            DICT_TOTAL[LABEL['label']] += 1
-                            DICT_RIGHT[LABEL['label']] += 1
-                if (image['PREDICTION_CHECK'] == ''):
-                    RIGHT+=1
-                    if ('BBOX_LABEL' in image):
-                        if((image['BBOX_LABEL']) == ''):
-                            image['BBOX_LABEL'] = '[]'
-                        BBOX_LABEL = json.loads(image['BBOX_LABEL'])
-                        for LABEL in BBOX_LABEL:
-                            if(not LABEL['label'] in DICT_TOTAL):
-                                DICT_TOTAL[LABEL['label']] = 0
-                                DICT_WRONG[LABEL['label']] = 0
-                                DICT_RIGHT[LABEL['label']] = 0
-                            DICT_TOTAL[LABEL['label']] += 1
-                            DICT_RIGHT[LABEL['label']] += 1                                 
-                if (image['PREDICTION_CHECK'] == 'NO_PREDICT'):
-                    WRONG+=1
-                    if ('BBOX_LABEL' in image):
-                        if((image['BBOX_LABEL']) == ''):
-                            image['BBOX_LABEL'] = '[]'
-                        BBOX_LABEL = json.loads(image['BBOX_LABEL'])
-                        for LABEL in BBOX_LABEL:
-                            if(not LABEL['label'] in DICT_TOTAL):
-                                DICT_TOTAL[LABEL['label']] = 0
-                                DICT_WRONG[LABEL['label']] = 0
-                                DICT_RIGHT[LABEL['label']] = 0
-                            DICT_TOTAL[LABEL['label']] += 1
-                            DICT_WRONG[LABEL['label']] += 1
-                     
-'''
-
-'''
-# 종합 정확도
-CURRENT_PRECISION = int((RIGHT*100)/(TOTAL+0.1))
-CURRENT_DATE = 'NOW'
-print('CURRENT_PRECISION : ',CURRENT_PRECISION)
-'''
+                GROUNDTRUTH_LABEL_SET.add(LABEL['label'])
+            for LABEL in BBOX_PREDICTION:
+                PREDICTION_LABEL_SET.add(LABEL['label'])
+                
+            for DISEASE in PREDICTION_LABEL_SET:
+                DICT_POSITIVETOTAL[DISEASE] += 1
+                if(DISEASE in GROUNDTRUTH_LABEL_SET):
+                    DICT_TRUEPOSITIVE[DISEASE] += 1
 
 
-    
-#print('CONFIRM_COUNT :',CONFIRM_COUNT)
-#print('PREDICT_COUNT :',PREDICT_COUNT)
-#print('NO_PREDICT_COUNT :',NO_PREDICT_COUNT)
-#print('NOT_CHECKED :',NOT_CHECKED)
-#print('TIME_RANGE : ',TIME_RANGE)
+for label in DICT_NEGATIVETOTAL:
+    DICT_PRECISION[label] = int((DICT_TRUEPOSITIVE[label]*100) / (DICT_POSITIVETOTAL[label] + 0.001))
+    if(DICT_POSITIVETOTAL[label] == 0):
+        print("%s Precision : %s %% (%s/%s)" % (label, 'N/A', str(DICT_TRUEPOSITIVE[label]), str(DICT_POSITIVETOTAL[label])))
+    else:
+        print("%s Precision : %s %% (%s/%s)" % (label, str(DICT_PRECISION[label]), str(DICT_TRUEPOSITIVE[label]), str(DICT_POSITIVETOTAL[label])))
+
+
+
+
+
 
 
