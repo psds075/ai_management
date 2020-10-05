8cbe5492e645f734f1e640a876d48731c9c2cda9
최선 <amorsun@naver.com>
2020-10-05 13:48:31 +0900

    MAIN : 버그 수정 및 정확도 기입

--- db_control/db_imagedata.py
+++ db_control/db_imagedata.py
@@ -4,7 +4,7 @@ import pymongo
 import os
 import pandas as pd
 import json
-from datetime import datetime
+import datetime
 
 # Connection
 myclient = pymongo.MongoClient("mongodb://ai:1111@dentiqub.iptime.org:27017/")
@@ -88,6 +88,7 @@ for image in imagedata.find(query):
     print(image['FILENAME'], image['DATASET_NAME'])
 '''
 
+'''
 #전체 DB에서 가로 세로 비율이 1.9 이하인 이미지를 찾아서 CONFIRM 수정하기
 
 # -*- coding: utf-8 -*-
@@ -128,6 +129,117 @@ for data in dataset.find({}):
                 #imagedata.update_one(query, newvalues)
 
 print('change', count)
+'''
+
+
+TODAY_STRING = datetime.datetime.now().strftime("%Y%m%d")
+
+DICT_TRUETOTAL = dict()
+DICT_NEGATIVETOTAL = dict()
+DICT_POSITIVETOTAL = dict()
+DICT_TRUENEGATIVE = dict()
+DICT_TRUEPOSITIVE = dict()
+
+DICT_SENSITIVITY = dict()
+DICT_SPECIFICITY = dict()
+DICT_PRECISION = dict()
+
+today = str(datetime.date.today())
+FROM_DATE = '2020-07-01'
+TO_DATE = today
+
+
+#Sensitivity 평가
+for image in imagedata.find({'CONFIRM_CHECK':'CONFIRM'}):
+    if 'BBOX_PREDICTION' in image:
+        if((image['TIMESTAMP'] > FROM_DATE) & (image['TIMESTAMP'] < TO_DATE)):
+            BBOX_LABEL_PREDICTION = json.loads(image['BBOX_PREDICTION'])
+            PREDICTED_LABEL_SET = set()
+            for LABEL in BBOX_LABEL_PREDICTION:
+                PREDICTED_LABEL_SET.add(LABEL['label'])
+            
+            BBOX_LABEL = json.loads(image['BBOX_LABEL'])
+            for LABEL in BBOX_LABEL:
+                if(not LABEL['label'] in DICT_TRUETOTAL):
+                    DICT_TRUETOTAL[LABEL['label']] = 0
+                    DICT_TRUEPOSITIVE[LABEL['label']] = 0
+                    DICT_NEGATIVETOTAL[LABEL['label']] = 0
+                    DICT_TRUENEGATIVE[LABEL['label']] = 0
+                    DICT_POSITIVETOTAL[LABEL['label']] = 0
+                DICT_TRUETOTAL[LABEL['label']] += 1
+                if(LABEL['label'] in PREDICTED_LABEL_SET):
+                    DICT_TRUEPOSITIVE[LABEL['label']] += 1
+
+for label in DICT_TRUETOTAL:
+    DICT_SENSITIVITY[label] = int((DICT_TRUEPOSITIVE[label]*100) / (DICT_TRUETOTAL[label] + 0.001))
+
+for label in sorted(DICT_SENSITIVITY.items(), key=lambda x: x[1], reverse=True):
+    label = label[0]
+    print("%s Sensitivity : %s %% (%s/%s)" % (label, str(DICT_SENSITIVITY[label]), str(DICT_TRUEPOSITIVE[label]), str(DICT_TRUETOTAL[label])))
+
+print()
+print()
+
+#Specificity 평가
+for image in imagedata.find({'CONFIRM_CHECK':'CONFIRM'}):
+    if 'BBOX_PREDICTION' in image:
+        if((image['TIMESTAMP'] > FROM_DATE) & (image['TIMESTAMP'] < TO_DATE)):
+            
+            BBOX_LABEL = json.loads(image['BBOX_LABEL'])
+            BBOX_PREDICTION = json.loads(image['BBOX_PREDICTION'])
+            GROUNDTRUTH_LABEL_SET = set()
+            PREDICTION_LABEL_SET = set()
+            for LABEL in BBOX_LABEL:
+                GROUNDTRUTH_LABEL_SET.add(LABEL['label'])
+            for LABEL in BBOX_PREDICTION:
+                PREDICTION_LABEL_SET.add(LABEL['label'])
+                
+            for DISEASE in list(DICT_TRUEPOSITIVE.keys()):
+                if(DISEASE not in GROUNDTRUTH_LABEL_SET):
+                    DICT_NEGATIVETOTAL[DISEASE] += 1
+                    if(DISEASE not in PREDICTION_LABEL_SET):
+                        DICT_TRUENEGATIVE[DISEASE] += 1
+
+for label in sorted(DICT_SENSITIVITY.items(), key=lambda x: x[1], reverse=True):
+    label = label[0]
+    DICT_SPECIFICITY[label] = int((DICT_TRUENEGATIVE[label]*100) / (DICT_NEGATIVETOTAL[label] + 0.001))
+    print("%s Specificity : %s %% (%s/%s)" % (label, str(DICT_SPECIFICITY[label]), str(DICT_TRUENEGATIVE[label]), str(DICT_NEGATIVETOTAL[label])))
+
+print()
+print()
+
+
+#Precision 평가
+for DISEASE in list(DICT_TRUEPOSITIVE.keys()):
+    DICT_TRUEPOSITIVE[DISEASE] = 0
+
+for image in imagedata.find({'CONFIRM_CHECK':'CONFIRM'}):
+    if 'BBOX_PREDICTION' in image:
+        if((image['TIMESTAMP'] > FROM_DATE) & (image['TIMESTAMP'] < TO_DATE)):
+            
+            BBOX_LABEL = json.loads(image['BBOX_LABEL'])
+            BBOX_PREDICTION = json.loads(image['BBOX_PREDICTION'])
+            GROUNDTRUTH_LABEL_SET = set()
+            PREDICTION_LABEL_SET = set()
+            for LABEL in BBOX_LABEL:
+                GROUNDTRUTH_LABEL_SET.add(LABEL['label'])
+            for LABEL in BBOX_PREDICTION:
+                PREDICTION_LABEL_SET.add(LABEL['label'])
+                
+            for DISEASE in PREDICTION_LABEL_SET:
+                DICT_POSITIVETOTAL[DISEASE] += 1
+                if(DISEASE in GROUNDTRUTH_LABEL_SET):
+                    DICT_TRUEPOSITIVE[DISEASE] += 1
+
+
+for label in sorted(DICT_SENSITIVITY.items(), key=lambda x: x[1], reverse=True):
+    label = label[0]
+    DICT_PRECISION[label] = int((DICT_TRUEPOSITIVE[label]*100) / (DICT_POSITIVETOTAL[label] + 0.001))
+    if(DICT_POSITIVETOTAL[label] == 0):
+        print("%s Precision : %s %% (%s/%s)" % (label, 'N/A', str(DICT_TRUEPOSITIVE[label]), str(DICT_POSITIVETOTAL[label])))
+    else:
+        print("%s Precision : %s %% (%s/%s)" % (label, str(DICT_PRECISION[label]), str(DICT_TRUEPOSITIVE[label]), str(DICT_POSITIVETOTAL[label])))
+
 
 
 
